a946b3125312d3f474d5468cd9ebf9ee24be41ec
Maré Geldenhuys <mareg@discovery.co.za>
2020-10-08 19:11:36 +0200

    Initial checkin

---
+++ app/src/main/java/web/geldenhuys/kotlinhabittrainer/MainActivity.kt
@@ -0,0 +1,11 @@
+package web.geldenhuys.kotlinhabittrainer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
