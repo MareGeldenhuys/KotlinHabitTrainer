e9e2a21d3cbb29a6ef4e8582dfe77f4b90020d11
Maré Geldenhuys <mareg@discovery.co.za>
2020-10-09 07:28:13 +0200

    Task 1

--- app/src/main/java/web/geldenhuys/kotlinhabittrainer/MainActivity.kt
+++ app/src/main/java/web/geldenhuys/kotlinhabittrainer/MainActivity.kt
@@ -1,11 +1,20 @@
 package web.geldenhuys.kotlinhabittrainer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_description.text = "Blah blah water is good for you"
+
+        iv_icon.setImageResource(R.drawable.water)
+        iv_title.text = "Water"
     }
 }
